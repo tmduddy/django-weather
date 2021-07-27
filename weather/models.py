5e97164bebad2165f9abdbc77452a2058a9c6237
tmduddy <login@tylerduddy.com>
2021-07-26 20:02:05 -0400

    add Report validation tests

--- weather/models.py
+++ weather/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class Report(models.Model):
     
     report_date = models.DateTimeField(auto_now_add=True)
     temperature = models.FloatField()
 
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    zipcode = models.CharField(max_length=5)
+    city = models.CharField(max_length=50, validators=[MaxLengthValidator(50)])
+    state = models.CharField(max_length=2, validators=[MaxLengthValidator(2)])
+    zipcode = models.CharField(max_length=5, validators=[MaxLengthValidator(5), MinLengthValidator(5)])
     
     class Weather(models.TextChoices):
         SUNNY = 'Sunny'
@@ -25,7 +26,7 @@ class Report(models.Model):
         default=Weather.SUNNY,
     )
 
-    description = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True, validators=[MaxLengthValidator(500)])
 
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,74 @@
+from datetime import timedelta
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Report
+
 
 # Create your tests here.
+class TestReportModel(TestCase):
+
+  def make_valid_report(self):
+    valid_report_params = {
+        "city": "Arlington",
+        "state": "VA",
+        "zipcode": "22201",
+        "temperature": 70.07,
+        "weather": Report.Weather.SUNNY,
+      }
+    
+    return Report.objects.create(**valid_report_params)
+
+  def test_report_date_now_when_blank(self):
+    """
+    Creating a new report with a blank report_date field should set the time to now
+    """
+    valid_report = self.make_valid_report()
+
+    report_date = valid_report.report_date
+    now = timezone.now()
+    difference = (now-report_date).seconds
+
+    self.assertEqual(difference, 0)
+
+  def test_reject_long_city(self):
+    """
+    Reporting with a city title length > 50 rejects the input
+    """
+    
+    valid_report = self.make_valid_report()
+
+    valid_report.city = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    self.assertRaises(ValidationError, valid_report.full_clean)
+
+  def test_reject_long_state(self):
+    """
+    Reporting with a city title length > 50 rejects the input
+    """
+    
+    valid_report = self.make_valid_report()
+
+    valid_report.state = "XYZ"
+    self.assertRaises(ValidationError, valid_report.full_clean)
+
+  def test_reject_long_zipcode(self):
+    """
+    Reporting with a city title length > 50 rejects the input
+    """
+    
+    valid_report = self.make_valid_report()
+
+    valid_report.zipcode = "000001"
+    self.assertRaises(ValidationError, valid_report.full_clean)
+
+  def test_reject_short_zipcode(self):
+    """
+    Reporting with a city title length > 50 rejects the input
+    """
+    
+    valid_report = self.make_valid_report()
+
+    valid_report.zipcode = "0000"
+    self.assertRaises(ValidationError, valid_report.full_clean)
